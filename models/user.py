60c92b9f294ed337d3af7f277d20b01a05129d54
Chris Johnson <christopherjohnson1@gmail.com>
2020-10-23 14:30:31 -0400

    wip

---
+++ models/user.py
@@ -0,0 +1,9 @@
+class User():
+    def __init__(self, id, user_name, email, password, first_name, last_name, bio):
+        self.id = id
+        self.user_name = user_name
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.bio = bio
